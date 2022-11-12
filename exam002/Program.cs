6258cf1aba77f8cc20e1a90334cc0409cf57aff0
azerbash <azerbash@AZERBASH-PC>
2022-11-12 13:40:57 +0330

    local git

---
+++ exam002/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace exam002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowWidth = 80;
+            Console.BufferWidth = 80;
+            while (true)
+            {
+                
+                int n = int.Parse(Console.ReadLine());
+                if (n == 13)
+                {
+                    break;
+                }
+                Console.Clear();
+                for (int i = 0; i < (2 * n - 1); i++)
+                {
+                    if (i < n)
+                    {
+                        for (int j = 0; j <= i; j++)
+                        {
+
+                            Console.Write("*");
+
+                        }
+                    }
+                    else
+                    {
+                        for (int j = (2 * n - 1); j > i; j--)
+                        {
+
+                            Console.Write("*");
+
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < j; j++)
+            //    {
+
+            //    }
+            //}
+
+        }
+    }
+}
